ea019ee10835916bf0b1062352c1921d9aa83799
Eldovyn <farras.pramudita@gmail.com>
2026-07-16 00:39:21 +0700

    fix: fix compose

--- backend/configs/mongo_db.py
+++ backend/configs/mongo_db.py
@@ -3,15 +3,14 @@ from mongoengine import connect
 
 def connect_db():
     host = os.getenv("MONGO_HOST", "mongo")
-    port = os.getenv("MONGO_PORT", "27017")
     user = os.getenv("MONGO_USER", "")
     password = os.getenv("MONGO_PASSWORD", "")
     db_name = os.getenv("MONGO_DB_NAME", "bagithr_indexer")
 
     if user and password:
-        uri = f"mongodb://{user}:{password}@{host}:{port}/{db_name}?authSource=admin"
+        uri = f"mongodb://{user}:{password}@{host}:27017/{db_name}?authSource=admin"
     else:
-        uri = f"mongodb://{host}:{port}/{db_name}"
+        uri = f"mongodb://{host}:27017/{db_name}"
 
-    print(f"[mongo_db] Connecting to MongoDB at {host}:{port}/{db_name}")
+    print(f"[mongo_db] Connecting to MongoDB at {host}:27017/{db_name}")
     connect(host=uri)
